e1e436519cb8c9ef8fea89f671e50c115deb99ab
Yogen'sWork <ygnrai@gmail.com>
2017-03-12 14:19:39 -0400

    customizing Docket bean example added

--- swagger-ui-example-part-2/src/main/java/com/eprogrammerz/examples/SwaggerConfiguration.java
+++ swagger-ui-example-part-2/src/main/java/com/eprogrammerz/examples/SwaggerConfiguration.java
@@ -2,11 +2,18 @@ package com.eprogrammerz.examples;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static com.google.common.collect.Lists.newArrayList;
 
 /**
  * Created by Yogen on 3/12/2017.
@@ -19,8 +26,29 @@ public class SwaggerConfiguration {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.eprogrammerz.examples.controllers"))
+                .paths(PathSelectors.ant("/movies/*"))
+                .build()
+                .apiInfo(this.movieApiInfo())
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET,
+                        newArrayList(new ResponseMessageBuilder()
+                                        .code(500)
+                                        .message("Internal Server Error")
+                                        .responseModel(new ModelRef("Error"))
+                                        .build(),
+                                new ResponseMessageBuilder()
+                                        .code(404)
+                                        .message("Document NOT FOUND!")
+                                        .build()));
+    }
+
+
+    private ApiInfo movieApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Movie API - build # 0.0.1")
+                .description("Provide the IDs in order to look for the content")
+                .version("0.1")
                 .build();
     }
 }
