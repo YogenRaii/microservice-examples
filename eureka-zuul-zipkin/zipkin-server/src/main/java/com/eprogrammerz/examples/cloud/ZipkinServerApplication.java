454fb8b28068ef46050e48c49d5adccae76831bc
Yogen Rai <yogen.rai.992@gmail.com>
2017-10-09 11:40:14 -0500

    Fixed Zipkin UI for tracing service calls.

--- eureka-zuul-zipkin/zipkin-server/src/main/java/com/eprogrammerz/examples/cloud/ZipkinServerApplication.java
+++ eureka-zuul-zipkin/zipkin-server/src/main/java/com/eprogrammerz/examples/cloud/ZipkinServerApplication.java
@@ -2,10 +2,13 @@ package com.eprogrammerz.examples.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
-@EnableZipkinStreamServer
+@EnableEurekaClient
+@EnableZipkinServer
 public class ZipkinServerApplication {
 
 	public static void main(String[] args) {
