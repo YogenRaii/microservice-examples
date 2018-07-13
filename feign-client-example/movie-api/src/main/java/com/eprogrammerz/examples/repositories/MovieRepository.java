b4e46f20ffba98d34275c237fe7bfdac9f12614f
Yogen Rai <yogen.rai.992@gmail.com>
2018-07-13 09:18:25 -0700

    removed duplicated example

--- feign-client-example/movie-api/src/main/java/com/eprogrammerz/examples/repositories/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eprogrammerz.examples.repositories;
-
-/**
- * Created by 542596 on 2/19/2017.
- */
-@FunctionalInterface
-public interface MovieRepository<T,R> {
-    R findOneById(T t);
-}
